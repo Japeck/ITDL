b1166b1b010cee5fb25d3922dbe795064bd930b7
hyugakasai <hyuga817@gmail.com>
2022-05-19 02:02:32 +0200

    Task 2 pretty much done

--- Assignment 1.R
+++ Assignment 1.R
@@ -1,6 +1,68 @@
 #libraries
 library(readr)
+library(stringr)
+library(lubridate)  
 
-#reading csv and creating dataset
+#reading csv and creating dataframe
 appstore_games <- read.csv("appstore_games.csv")
+
+#removing columns URL and Name
+appstore_games = subset(appstore_games, select = -c(URL, Name))
+
+#creating new binary column and removing Subtitle column
+appstore_games$Recorded.Subtitle <- ifelse(appstore_games$Subtitle>0, 1, 0)
+appstore_games = subset(appstore_games, select = -c(Subtitle))
+
+#divide text, make it into a new dataframe, and convert them into numeric values
+IAP <- str_split(appstore_games$In.app.Purchases, ',', simplify=TRUE)
+IAP.df <- as.data.frame(IAP)
+IAP.df = data.frame(apply(IAP.df, 2, function(x) as.numeric(as.character(x))))
+
+#count occurrences, then get min, max, sum and mean as new columns
+appstore_games$IAP.Values <- apply(IAP.df>0, MARGIN=1, FUN=sum, na.rm=TRUE)
+appstore_games$Minimum.IAP <- ifelse(appstore_games$IAP.Values>0, apply(IAP.df, MARGIN=1, FUN=min, na.rm=TRUE), NA)
+appstore_games$Maximum.IAP <- ifelse(appstore_games$IAP.Values>0, apply(IAP.df, MARGIN=1, FUN=max, na.rm=TRUE), NA)
+appstore_games$Sum.IAP <- ifelse(appstore_games$IAP.Values>0, apply(IAP.df, MARGIN=1, FUN=sum, na.rm=TRUE), NA)
+appstore_games$Average.IAP <- ifelse(appstore_games$IAP.Values>0, round(apply(IAP.df, MARGIN=1, FUN=mean, na.rm=TRUE), 2), NA)
+
+#counting words by splitting string by spaces and removing the Description column
+appstore_games$Description.Word.Count <- str_count(appstore_games$Description, '\\W+')
+appstore_games = subset(appstore_games, select = -c(Description))
+
+#creating a new column of 2 categories by making a dataframe of the developers frequencies and merging them
+Developers <- table(appstore_games$Developer)
+Developers.df <- as.data.frame(Developers)
+colnames(Developers.df) <- c('Developer', 'Developer.Game.Count')
+Developers.df$Developer.Category <- ifelse(Developers.df$Developer.Game.Count<4, 'Newbie', 'Professional')
+appstore_games = merge(appstore_games, Developers.df, by='Developer')
+appstore_games = subset(appstore_games, select = -c(Developer.Game.Count))
+
+#a Second Age Rating column to divide games into just 4+ and 9+
+appstore_games$Second.Age.Rating <- ifelse(appstore_games$Age.Rating=='4+', '4+', '9+')
+
+#new Number.of.Languages column divided into Single or Many and games with no Languages or categorized as single
+appstore_games$Number.of.Languages <- ifelse(nchar(appstore_games$Languages)<=2, 'Single', 'Many')
+#new Is.Available.In.English column divided into Yes or No
+appstore_games$Is.Available.In.English <- ifelse(lengths(strsplit(appstore_games$Languages, 'EN'))==2 | startsWith(appstore_games$Languages, 'EN') | endsWith(appstore_games$Languages, 'EN'), 'Yes', 'No')
+
+#removing Primary.Genre
+appstore_games = subset(appstore_games, select = -c(Primary.Genre))
+
+#creating new Number.of.Genres column
+appstore_games$Number.of.Genres <- lengths(strsplit(appstore_games$Genres, ','))
+
+#converting Original.Release.Date to date format and creating new Release.Month column
+appstore_games$Original.Release.Date = as.Date(appstore_games$Original.Release.Date, "%d/%m/%Y")
+appstore_games$Release.Month <- strftime(appstore_games$Original.Release.Date,"%m")
+
+#converting Current.Version.Release.Date to date format and creating new Elapsed.Months column
+appstore_games$Current.Version.Release.Date = as.Date(appstore_games$Current.Version.Release.Date, "%d/%m/%Y")
+data_collection_date <- as.Date('2019-08-03')
+appstore_games$Elapsed.Months <- interval(appstore_games$Current.Version.Release.Date, data_collection_date) %/% months(1) 
+
+#creating new Game.Free column
+appstore_games$Game.Free <- ifelse(appstore_games$IAP.Values==0, 1, 0)
+
+#creating new Categorical.Rating.Count column
+appstore_games$Categorical.Rating.Count <- ifelse(appstore_games$User.Rating.Count<median(appstore_games$User.Rating.Count, na.rm=TRUE), 'Low', 'High')
 
